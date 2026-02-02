c92fc49dff4b545b2caa1917ae58293c6e4abb4f
Riyaz Ahuja <115503524+riyazahuja@users.noreply.github.com>
2026-02-01 23:38:49 -0500

    Change VerifiedAgora dependency to main branch

--- lakefile.lean
+++ lakefile.lean
@@ -8,7 +8,7 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "v4.17.0"
 
 require VerifiedAgora from git
-  "https://github.com/stagiralabs/VerifiedAgora.git" @ "v4.17.0"
+  "https://github.com/stagiralabs/VerifiedAgora.git" @ "main"
 
 @[default_target]
 lean_lib «Mathlib.InformationTheory» where
