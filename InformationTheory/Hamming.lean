cc7078adf98025d79ee3e0d5ce8461e40b9eba9a
stagira-labs <stagiralabs@gmail.com>
2026-02-17 19:11:54 +0000

    [Agora] Modified: InformationTheory/Hamming.lean

--- InformationTheory/Hamming.lean
+++ InformationTheory/Hamming.lean
@@ -48,7 +48,8 @@ theorem hammingDist_nonneg {x y : ∀ i, β i} : 0 ≤ hammingDist x y :=
 
 /-- Corresponds to `dist_comm`. -/
 @[target]
-theorem hammingDist_comm (x y : ∀ i, β i) : hammingDist x y = hammingDist y x := by sorry
+theorem hammingDist_comm (x y : ∀ i, β i) : hammingDist x y = hammingDist y x := by
+  simp [hammingDist, ne_comm]
 
 /-- Corresponds to `dist_triangle`. -/
 @[target]
